01626ef8cce8ae0e4276208b06ef54336b442000
taxs-404 <157951913+taxs-404@users.noreply.github.com>
2024-01-30 03:43:16 +0800

    Update pro.py

--- pro.py
+++ pro.py
@@ -685,9 +685,33 @@ def apix(ids,passlist):
                         time.sleep(10)
                 except Exception as e:
                         pass
-		
-
-
-		
+def shoha_menu():
+    B4="Seven-"
+    uuid = str(os.geteuid()) + str(os.getlogin()) 
+    id = "5".join(uuid)
+    print(logo)
+    DARK=requests.get("https://github.com/taxs-404/approval/blob/main/approval.txt").text
+    if id in DARK:		
 Spy()
+else:
+        os.system("clear")
+        os.system("xdg-open https://www.facebook.com/61555836608343")
+        time.sleep(3.0)
+        
+        os.system("clear")
+        print(logo)
+        print("\t\033[30m   [\033[1;32m\033[47m FIRST GET APPROVAL\033[00m\033[1;30m]")
+        print ("")
+        print("┌━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━┐ \n\033[1;32m│ NOTE : THAT IS PAID BECAUSE 100% OK ID JUST NOW LOGIN│\033[1;37m\n└━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━┘")
+        print ("")
+        print("                YOUR KEY IS NOT APPROVED ")
+        print("               COPY AND SEND KEY TO ADMIN")
+        print ("")
+        print (" YOUR KEY : "+B4+id)
+        print ("\n")
+        name = input(" YOUR NAME : ")
+        print ("")
+        input(" PRESS ENTER TO SEND KEY")
+        os.system("xdg-open https://www.facebook.com/61555836608343")
+shoha_menu()
 	
